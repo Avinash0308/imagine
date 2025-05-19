eee3dd2f737916ba2ca1434323d6465bbd023b8e
Avinash Agrawal <avinash03082003@gmail.com>
2025-05-20 01:11:09 +0530

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,20 @@
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  )
+}
+
+export default Home
+
+// import {UserButton } from '@clerk/nextjs';
+
+
+// export default function Home() {
+//   return (
+//     <div>
+//         <UserButton/>
+//     </div>
+//   );
+// }

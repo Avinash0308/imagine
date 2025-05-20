52eaee4f35832b65f659983b74e890883392f0c2
Avinash Agrawal <avinash03082003@gmail.com>
2025-05-20 23:58:20 +0530

    Update public routes.

--- middleware.ts
+++ middleware.ts
@@ -1,41 +1,7 @@
-// import { clerkMiddleware } from '@clerk/nextjs/server';
- 
-// export default clerkMiddleware()
- 
-// export const config = {
-//   matcher: [
-//     '/((?!.*\\..*|_next).*)', // Don't run middleware on static files
-//     '/', // Run middleware on index page
-//     '/(api|trpc)(.*)'], // Run middleware on API routes
-// };
-
-
-// import { clerkMiddleware, createRouteMatcher  } from "@clerk/nextjs/server";
-
-// const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
-
-// export default clerkMiddleware((auth, request) => {
-//   if(!isPublicRoute(request)) {
-//     auth().protect();
-//   }
-// });
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API routes
-//     '/(api|trpc)(.*)',
-//   ],
-// };
-
-
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk'],
-  //ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/profile"]
-  //publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
 });
  
 export const config = {
